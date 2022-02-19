d510b7a90618b7f1fff961300b529e056122bc60
Adam Sasine <adam.sasine@gmail.com>
2022-02-18 19:40:15 -0500

    Added halting behavior to fallback node

--- Runtime/Nodes/Control/FallbackNode.cs
+++ Runtime/Nodes/Control/FallbackNode.cs
@@ -4,15 +4,19 @@ namespace BehaviorTree.NET.Nodes.Control
 {
     public class FallbackNode : ControlNode
     {
+        private int currentRunningIndex;
+
         public FallbackNode(IEnumerable<INode> children)
             : base(children)
         {
+            this.currentRunningIndex = 0;
         }
 
         public override NodeStatus Tick()
         {
-            foreach (var child in this.Children)
+            for (int i = 0; i < this.Children.Count; i++)
             {
+                var child = this.Children[i];
                 var status = child.Tick();
                 switch (status)
                 {
@@ -20,6 +24,16 @@ namespace BehaviorTree.NET.Nodes.Control
                         this.Halt();
                         return status;
                     case NodeStatus.RUNNING:
+                        if (i != this.currentRunningIndex)
+                        {
+                            // a new child is running, all children after i, up to the previously running child (inclusive) should be halted
+                            for (int j = i + 1; j <= this.currentRunningIndex; j++)
+                            {
+                                this.HaltChild(j);
+                            }
+                        }
+
+                        this.currentRunningIndex = i;
                         return status;
                     case NodeStatus.FAILURE:
                         continue;
@@ -28,6 +42,12 @@ namespace BehaviorTree.NET.Nodes.Control
 
             this.Halt();
             return NodeStatus.FAILURE;
+        }
+
+        public override void Halt()
+        {
+            this.currentRunningIndex = 0;
+            base.Halt();
         }
     }
 }
